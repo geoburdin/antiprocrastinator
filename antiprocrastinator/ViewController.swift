11fd8699a97fe06dc52714cec5a16ef855617a74
GeorgiiBurdinComo <georgii.burdin@gocomo.io>
2024-06-11 20:12:58 +0200

    Initial Commit

---
+++ antiprocrastinator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  antiprocrastinator
+//
+//  Created by Georgii Burdi on 11.06.24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
